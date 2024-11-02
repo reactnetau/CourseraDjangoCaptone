0b0decd963ca3438e863d59af7543e5a2a885339
reactnetau <polyrhythmm@gmail.com>
2024-11-03 03:21:46 +1100

    Add testing

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,6 +6,9 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
 
+    def get_item(self):
+        return f"{self.title} : {self.price}"
+
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
